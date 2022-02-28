29301e9d7a8b440089e4e4c409e427930af8b63a
GabrielOliveira-mensurepec <gabriel.oliveira@mensurepec.com>
2022-02-28 00:36:22 -0400

    compilador test e mudanças nomes variaveis

--- listObject/main.cpp
+++ listObject/main.cpp
@@ -1,102 +1,115 @@
-typedef int TIPOCHAVE;
-#define MAX 50
+typedef int keyType;
+#define maxObject 50
 
 typedef struct
 {
-    TIPOCHAVE chave;
+    keyType key;
     // outros campos
-} REGISTRO;
+} objecType;
 
 typedef struct
 {
-    REGISTRO A[MAX];
-    int nroElem;
-} LISTA;
+    objecType objectList[maxObject];
+    int elementNumber;
+} listType;
 
 #include <iostream>
 // #include <conio.h>
 // Fun��es para manipular a lista sequencial
-void initLista(LISTA *l);                       // Inicializa��o da lista com Zero elementos
-int qtdElem(LISTA *l);                          // Retorna a qtde de elementos na lista
-void imprimeLista(LISTA *l);                    // Imprime o conte�do da lista
-bool insertLista(LISTA *l, REGISTRO rg, int i); // insere um elemento na posi��o desejada.
-                                                // Se n�o for permitido retorna false.
-int buscaElem(LISTA *l, TIPOCHAVE ch); // busca o elemento na lista
-                                       // Se encontar retorna a posi��o Sen�o retorna -1
+void displayMenu();
+void initList(listType *l);                        // Inicializa��o da lista com Zero elementos
+int elementsQuantity(listType *l);                 // Retorna a qtde de elementos na lista
+void printList(listType *l);                       // Imprime o conte�do da lista
+bool insertList(listType *l, objecType rg, int i); // insere um elemento na posi��o desejada.
+                                                   // Se n�o for permitido retorna false.
+int findElement(listType *l, keyType ch);          // busca o elemento na lista
+                                                   // Se encontar retorna a posi��o Sen�o retorna -1
 
 using namespace std;
 
 int main()
 {
-    LISTA lista;
-    initLista(&lista);
-    // cout <<"qtd elementos(main): "<<lista.nroElem;
-    cout << "qtd elementos na lista: " << qtdElem(&lista) << endl;
+    displayMenu();
+    listType lista;
+    initList(&lista);
+    // cout <<"qtd elementos(main): "<<lista.elementNumber;
+    cout << "qtd elementos na lista: " << elementsQuantity(&lista) << endl;
     // Adicionar manualmente os elementos na lista
-    lista.A[0].chave = 10;
-    lista.A[1].chave = 20;
-    lista.A[2].chave = 30;
-    imprimeLista(&lista);
+    lista.objectList[0].key = 10;
+    lista.objectList[1].key = 20;
+    lista.objectList[2].key = 30;
+    printList(&lista);
     cout << endl;
-    REGISTRO aux;
+    objecType aux;
     bool result;
-    aux.chave = 50;
-    result = insertLista(&lista, aux, 1);
+    aux.key = 50;
+    result = insertList(&lista, aux, 1);
     cout << result << endl;
-    imprimeLista(&lista);
+    printList(&lista);
 
-    int x = buscaElem(&lista, 200);
+    int x = findElement(&lista, 200);
     cout << endl
          << "Valor encontador na pos.: " << x << endl;
     return 0;
 }
 
 // Fun��es//
-void initLista(LISTA *l)
+void initList(listType *l)
 {
-    l->nroElem = 3;
-    // cout << "qtd elementos(func): " <<l->nroElem <<endl;
+    l->elementNumber = 3;
+    // cout << "qtd elementos(func): " <<l->elementNumber <<endl;
 }
 
-int qtdElem(LISTA *l)
+int elementsQuantity(listType *l)
 {
-    return l->nroElem;
+    return l->elementNumber;
 }
 
-void imprimeLista(LISTA *l)
+void printList(listType *l)
 {
-    int i; // manipular o indice do vetor da estrutura onde est� armazenado a informa��o (chave)
+    int i; // manipular o indice do vetor da estrutura onde est� armazenado a informa��o (key)
     cout << "Lista: ";
-    for (i = 0; i < l->nroElem; i++)
+    for (i = 0; i < l->elementNumber; i++)
     {
-        cout << l->A[i].chave << " ";
+        cout << l->objectList[i].key << " ";
     }
 }
-bool insertLista(LISTA *l, REGISTRO rg, int i)
+bool insertList(listType *l, objecType rg, int i)
 {
     // valida��o da posi��o informada e se a lista est� cheia
-    if (l->nroElem == MAX || i < 0 || i > l->nroElem)
+    if (l->elementNumber == maxObject || i < 0 || i > l->elementNumber)
         return false;
 
     int j; // var. para auxiliar na manipula��o dos �ndice
     // Deslocando os elementos para direita
-    for (j = l->nroElem; j > i; j--)
-        l->A[j] = l->A[j - 1];
+    for (j = l->elementNumber; j > i; j--)
+        l->objectList[j] = l->objectList[j - 1];
 
-    // Armazenando a chave na posi��o desejada
-    l->A[i] = rg;
-    l->nroElem++;
+    // Armazenando a key na posi��o desejada
+    l->objectList[i] = rg;
+    l->elementNumber++;
     return true;
 }
-int buscaElem(LISTA *l, TIPOCHAVE ch)
+int findElement(listType *l, keyType ch)
 {
     int i = 0; // var. para auxiliar na manipula��o dos �ndice
-    while (i < l->nroElem)
+    while (i < l->elementNumber)
     {
-        if (ch == l->A[i].chave)
+        if (ch == l->objectList[i].key)
             return i; // retorna a posi��o do elemento
         else
             i++;
     }
     return -1; // nenhum elemento encontrado
+}
+void displayMenu()
+{
+    cout << "------------------------\n";
+    cout << "\tMenu\n";
+    cout << "------------------------\n";
+    cout << "1 - Difficulty\n";
+    cout << "2 - Sound";
+    cout << "3 - Back\n";
+    cout << "3 - Back\n";
+    cout << "Digite 1,2,3 ou 4: \n\n";
 }
