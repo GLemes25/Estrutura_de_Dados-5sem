92de150a21ce574a9f6b8f0800047753d7c0178b
GabrielOliveira-mensurepec <gabriel.oliveira@mensurepec.com>
2022-03-03 12:38:00 -0400

    Escolha de Menu e ordenação

--- listObject/main.cpp
+++ listObject/main.cpp
@@ -4,98 +4,144 @@ typedef int keyType;
 typedef struct
 {
     keyType key;
-    // outros campos
 } objecType;
 
 typedef struct
 {
     objecType objectList[maxObject];
     int elementNumber;
-} listType;
+} objectListType;
 
 #include <iostream>
+#include <bits/stdc++.h>
 // #include <conio.h>
-// Fun��es para manipular a lista sequencial
+// Funções para manipular a lista sequencial
 void displayMenu();
-void initList(listType *l);                        // Inicializa��o da lista com Zero elementos
-int elementsQuantity(listType *l);                 // Retorna a qtde de elementos na lista
-void printList(listType *l);                       // Imprime o conte�do da lista
-bool insertList(listType *l, objecType rg, int i); // insere um elemento na posi��o desejada. // Se n�o for permitido retorna false.
-int findElement(listType *l, keyType ch);          // busca o elemento na lista
-                                                   // Se encontar retorna a posi��o Sen�o retorna -1
+objectListType orderList(objectListType *list);
+bool confirm();
+void initList(objectListType *list);                   // Inicialização da lista com Zero elementos
+void elementsQuantity(objectListType *list);           // Retorna a qtde de elementos na lista
+void printList(objectListType *list);                  // Imprime o conteúdo da lista
+void insertList(objectListType *list, objecType rg);   // insere um elemento na posição desejada. // Se não for permitido retorna false.
+int findIndexByKey(objectListType *list, keyType key); // busca o elemento na lista
+                                                       // Se encontar retorna a posição Senão retorna -1
 
 using namespace std;
 
 int main()
 {
-    displayMenu();
-    listType lista;
-    initList(&lista);
-    // cout <<"qtd elementos(main): "<<lista.elementNumber;
-    cout << "qtd elementos na lista: " << elementsQuantity(&lista) << endl;
-    // Adicionar manualmente os elementos na lista
-    lista.objectList[0].key = 10;
-    lista.objectList[1].key = 20;
-    lista.objectList[2].key = 30;
-    printList(&lista);
-    cout << endl;
-    objecType aux;
-    bool result;
-    aux.key = 50;
-    result = insertList(&lista, aux, 1);
-    cout << result << endl;
-    printList(&lista);
+    objectListType list;
+    int option;
+    bool doAgain = true;
 
-    int x = findElement(&lista, 200);
-    cout << endl
-         << "Valor encontador na pos.: " << x << endl;
+    // lista.objectList[0].key = 10;
+    // lista.objectList[1].key = 20;
+    // lista.objectList[2].key = 30;
+
+    do
+    {
+        /* code */
+
+        displayMenu();
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+        {
+            int firstKey;
+            initList(&list);
+            cout << "digite o primeiro item da lista " << endl;
+            cin >> firstKey;
+            list.objectList[0].key = firstKey;
+            break;
+        }
+
+        case 2:
+        {
+            elementsQuantity(&list);
+            break;
+        }
+        case 3:
+        {
+            printList(&list);
+            break;
+        }
+        case 4:
+        {
+            int index;
+            objecType aux;
+            cout << "Digite a achave :" << endl;
+            cin >> aux.key;
+            insertList(&list, aux);
+            list = orderList(&list);
+            break;
+        }
+        case 5:
+        {
+            keyType key;
+            cout << "Digite a chave :" << endl;
+            cin >> key;
+            findIndexByKey(&list, key);
+            // orderList(&list);
+            break;
+        }
+        default:
+            cout << "Opção invalida" << endl;
+        }
+        doAgain = confirm();
+
+    } while (doAgain == true);
+
     return 0;
 }
 
 // Fun��es//
-void initList(listType *l)
+void initList(objectListType *list)
 {
-    l->elementNumber = 3;
-    // cout << "qtd elementos(func): " <<l->elementNumber <<endl;
+    list->elementNumber = 1;
 }
 
-int elementsQuantity(listType *l)
+void elementsQuantity(objectListType *list)
 {
-    return l->elementNumber;
+    cout << "qtd elementos na lista: " << list->elementNumber << endl;
 }
 
-void printList(listType *l)
+void printList(objectListType *list)
 {
-    int i; // manipular o indice do vetor da estrutura onde est� armazenado a informa��o (key)
-    cout << "Lista: ";
-    for (i = 0; i < l->elementNumber; i++)
+    int i; // manipular o indice do vetor da estrutura onde está armazenado a informação (key)
+    cout << "Lista: [ ";
+    for (i = 0; i < list->elementNumber; i++)
     {
-        cout << l->objectList[i].key << " ";
+        cout << list->objectList[i].key;
+
+        i == list->elementNumber - 1 ? cout << " ] " : cout << " - ";
+    }
+    cout << endl;
+}
+void insertList(objectListType *list, objecType key)
+{
+    int position = list->elementNumber;
+    // validação da posição informada e se a lista está cheia
+    if (list->elementNumber != maxObject || position > 0)
+    {
+        int j; // var. para auxiliar na manipul1ação dos índice
+        // Deslocando os elementos para direita
+        for (j = list->elementNumber; j > position; j--)
+            list->objectList[j] = list->objectList[j - 1];
+
+        // Armazenando a key na posição desejada
+        list->objectList[position] = key;
+        list->elementNumber++;
     }
 }
-bool insertList(listType *l, objecType rg, int i)
+int findIndexByKey(objectListType *list, keyType key)
 {
-    // valida��o da posi��o informada e se a lista est� cheia
-    if (l->elementNumber == maxObject || i < 0 || i > l->elementNumber)
-        return false;
-
-    int j; // var. para auxiliar na manipula��o dos �ndice
-    // Deslocando os elementos para direita
-    for (j = l->elementNumber; j > i; j--)
-        l->objectList[j] = l->objectList[j - 1];
-
-    // Armazenando a key na posi��o desejada
-    l->objectList[i] = rg;
-    l->elementNumber++;
-    return true;
-}
-int findElement(listType *l, keyType ch)
-{
-    int i = 0; // var. para auxiliar na manipula��o dos �ndice
-    while (i < l->elementNumber)
+    int i = 0; // var. para auxiliar na manipulação dos índice
+    while (i < list->elementNumber)
     {
-        if (ch == l->objectList[i].key)
-            return i; // retorna a posi��o do elemento
+        if (key == list->objectList[i].key)
+            return i; // retorna a posição do elemento
         else
             i++;
     }
@@ -106,10 +152,46 @@ void displayMenu()
     cout << "-------------------------------------" << endl;
     cout << "\tMenu" << endl;
     cout << "-------------------------------------" << endl;
-    cout << "1 - Mostrar Quantidade de Elementos" << endl;
-    cout << "2 - Mostrar Lista";
-    cout << "3 - Inserir Item na Lista" << endl;
-    cout << "4 - Achar Elementos " << endl;
-    cout << "Digite 1,2,3 ou 4: " << endl
+    cout << "1 - Iniciar Lista" << endl;
+    cout << "2 - Mostrar Quantidade de Elementos" << endl;
+    cout << "3 - Mostrar Lista" << endl;
+    cout << "4 - Inserir Item na Lista" << endl;
+    cout << "5 - Achar Elementos " << endl;
+    cout << "Digite 1,2,3,4 ou 5: " << endl
          << endl;
+}
+bool confirm()
+{
+    char confirm;
+    do
+    {
+        cout << "Deseja Continar? [y/n]" << endl;
+        cin >> confirm;
+    } while (!cin.fail() && confirm != 'y' && confirm != 'n');
+    return (confirm == 'y' ? true : false);
+}
+objectListType orderList(objectListType *list)
+{
+    int aux;
+    objectListType orderedList;
+    orderedList.elementNumber = list->elementNumber;
+
+    for (int i = 0; i < list->elementNumber; i++)
+    {
+        for (int j = i + 1; j < list->elementNumber; j++)
+        {
+            if (list->objectList[i].key > list->objectList[j].key)
+            {
+                aux = list->objectList[i].key;
+                list->objectList[i].key = list->objectList[j].key;
+                list->objectList[j].key = aux;
+                orderedList.objectList[j].key = aux;
+                orderedList.objectList[i].key = list->objectList[i].key;
+            }
+        }
+    }
+
+    printList(&orderedList);
+
+    return orderedList;
 }
