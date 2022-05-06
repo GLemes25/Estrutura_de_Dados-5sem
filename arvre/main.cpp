66e69b35648119e4ab58fefc8a943269b14ede63
GLemes25 <gabriellemes924@gmail.com>
2022-05-06 12:35:31 -0400

    faz

--- arvre/main.cpp
+++ arvre/main.cpp
@@ -12,16 +12,85 @@ typedef struct tree
 
 typedef NodeType *PointerType;
 
+// inicialização da abb
 PointerType init()
 {
-    PointerType treeRoot; // raiz da arvore
-    treeRoot = init();
+    return NULL;
+}
+
+// Inserir um no
+PointerType insert(PointerType node, int key)
+{
+    // SE: A raiz é NULL instala o nó na raiz da arvore
+    // SENAO:
+    //    SE: A chave a ser inserida for menor que a raiz
+    //        Insere na subarvore da esquerda
+    //    SENAO:
+    //        Insere na subarvore da direita
+
+    if (node == NULL)
+    {
+        node = (PointerType)malloc(sizeof(PointerType));
+        node->info = key;
+        node->left = NULL;
+        node->right = NULL;
+    }
+    else if (key < node->info)
+    {
+        node->left = insert(node->left, key);
+    }
+    else if (key > node->info)
+    {
+        node->right = insert(node->right, key);
+    }
+    else
+    {
+        cout << "Chave Invalida! \n\n";
+    }
+
+    return node;
+}
+
+PointerType find(PointerType node, int key)
+{
+    while (node != NULL)
+    {
+        if (node->info == key)
+        {
+            return node;
+        }
+        else if (node->info > key)
+        {
+            node = node->left;
+        }
+        else
+        {
+            node->right;
+        }
+    }
+    return NULL;    //Não encontrou 
+}
+
+void show(PointerType node)
+{
+
+    cout << "\t" << node->info << "\n";
+    cout << node->left->info;
+    node->right ? cout << "\t\t" << (node->right->info) << "\n" : cout << "\t\t"
+                                                                       << "NULL"
+                                                                       << "\n";
 }
 
 int main()
 {
-    int option;
-    bool initiate = false;
+    PointerType treeRoot; // raiz da arvore
+    treeRoot = init();
+    treeRoot = insert(treeRoot, 10);
+    treeRoot = insert(treeRoot, 1);
+    treeRoot = insert(treeRoot, 17);
+    show(treeRoot);
+
+
 
     return 0;
 }
